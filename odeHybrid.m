2b7142139f4f809633db06515becc9c7ad77763c
jonreal:anarres <jdrealmuto@gmail.com>
2025-06-03 12:33:07 -0700

    Added Neural ODE modeling

--- odeHybrid.m
+++ odeHybrid.m
@@ -11,6 +11,8 @@ function [T,X] = odeHybrid(model,tspan,x0,options)
   X = zeros(numel(tspan),numel(x0));
   m = 1;
 
+
+%  figure;
   fprintf('Integrating HDS .')
   while(1)
     if isempty(t0) || (t0 > tspan(end))
@@ -25,6 +27,10 @@ function [T,X] = odeHybrid(model,tspan,x0,options)
     m = find(strcmp(edge,{model.modes.domain}));
     X(i0:(i0+numel(tout)-1),:) = x;
     T(i0:(i0+numel(tout)-1),:) = tout;
+
+%    plot(T(1:i0+numel(tout)-1),X(1:i0+numel(tout)-1,:));
+  %  pause(0.01);
+
   end
   fprintf('done.\n\n');
 end
@@ -43,10 +49,12 @@ function [edge,x,tspan,t0,x0] = SimulateToNextEvent(m,tt,x0);
 
   dt = tt(2)-tt(1);
   options = odeset('Events',m.guards, ...
-                   'MaxStep',dt);
-  %[x0,xp0] = decic(m.flow,tt(1),x0,ones(size(x0)),zeros(size(x0)),[]);
-  [x0,xp0] = decic(m.flow,tt(1),x0,[0;0;1;0;0;1;1;1;1;0;0], ...
-                    zeros(size(x0)),[]);
+                   'MaxStep',dt, ...
+                   'RelTol',1e-9,'AbsTol',1e-12);
+  [x0,xp0] = decic(m.flow,tt(1),x0,ones(size(x0)),zeros(size(x0)),[]);
+
+  %[x0,xp0] = decic(m.flow,tt(1),x0,[0;0;1;0;0;1;1;1;1;0;0], ...
+  %                  zeros(size(x0)),[]);
 
   % Check if initial coniditions are in the guard sets, if so, simulate foward
   % one time step and trigger the transition manually.
@@ -78,7 +86,7 @@ function [edge,x,tspan,t0,x0] = SimulateToNextEvent(m,tt,x0);
     t0 = [];
     return;
   elseif tspan(end) >= tt(end)
-    x = interp1(tspan,x,tt,'pchip');
+    x = interp1(tspan,x,tt,'linear');
     tspan = tt(1:end);
     edge = [];
     t0 = [];
@@ -89,35 +97,43 @@ function [edge,x,tspan,t0,x0] = SimulateToNextEvent(m,tt,x0);
     te = te(end);
   end
 
-  % find tspan at ie
   [~,i0] = min(abs(tspan-te));
-  tspan(i0:end) = [];
-  x(i0:end,:) = [];
-
-  % find next time step closest to the event
-  [~,i0] = min(abs((tt>te).*tt - te));
-
-  % simulate foward two time step
-  [x0,xp0] = decic(m.flow,tspan(end), ...
-                    x(end,:)',ones(size(x0)),zeros(size(x0)),[]);
-  i0 = min(i0,numel(tt)-1);
-  options.Events = [];
-
-  [t_,x_] = ode15i(m.flow,[tspan(end);tt(i0+1)],x0,xp0,options);
-  [t_,iunique,~] = unique(t_);
-  x_ = x_(iunique,:);
-
-  % jump map
-  x0 = m.jumpmaps{ie}(t_(end),x_(end,:));
-
-  % interp flow to match timesteps
-  x = interp1([tspan; t_(2:end)],[x; x_(2:end,:)],tt(1:i0+1),'pchip');
-  x = x(1:end-1,:);
-  tspan = tt(1:i0);
   t0 = tt(i0+1);
-
-  % next mode
+  x0 = m.jumpmaps{ie}(te,x(end,:));
   edge = m.edges{ie};
+
+
+  %% find tspan at ie
+  %[~,i0] = min(abs(tspan-te));
+  %tspan(i0:end) = [];
+  %x(i0:end,:) = [];
+
+  %% jump map
+  %x0 = m.jumpmaps{ie}(t_(end),x_(end,:));
+  %% find next time step closest to the event
+  %[~,i0] = min(abs((tt>te).*tt - te));
+
+  %% simulate foward onto the next grid timestep
+  %[x0,xp0] = decic(m.flow,tspan(end), ...
+  %                  x(end,:)',ones(size(x0)),zeros(size(x0)),[]);
+  %i0 = min(i0,numel(tt)-1);
+  %options.Events = [];
+
+  %%[t_,x_] = ode15i(m.flow,[tspan(end);tt(i0+1)],x0,xp0,options);
+  %%[t_,iunique,~] = unique(t_);
+  %%x_ = x_(iunique,:);
+
+  %%% jump map
+  %%x0 = m.jumpmaps{ie}(t_(end),x_(end,:));
+
+  %%% interp flow to match timesteps
+  %%x = interp1([tspan; t_(2:end)],[x; x_(2:end,:)],tt(1:i0+1),'linear');
+  %%x = x(1:end-1,:);
+  %%tspan = tt(1:i0);
+  %%t0 = tt(i0+1);
+
+  %% next mode
+  %edge = m.edges{ie};
 
 end
 
---
+++ train_hnode.m
@@ -0,0 +1,115 @@
+% Time
+T = 10;
+dt = 0.1;
+t = 0:dt:(T-dt);
+nsteps = numel(t);
+
+% Training Parameters
+nSamples = 1;
+nTrainingRuns = 10;
+Epochs = 1000;
+LearningRate = 1e-4;
+
+use_adjoint = true;
+
+% Toy model
+dx =@(t,x,u,p) [ ...
+    x(2);
+    1/p(1) * (u - p(2)*x(2) - (p(3)*x(2) + p(4)*x(1) + p(5)*x(1)^2))];
+
+% Use step inputs
+u =@(t, ts, a) a * (t >= ts);
+
+%cmap = cbrewer('div','Spectral',nSamples);
+%cmap(cmap > 1) = 1;
+cmap = colormap(copper(nSamples));
+
+p = [0.5, 0.5, -0.03, 0.1, 0.5]; % Parameters
+
+X_train = cell(size(nSamples, 1), 1);
+Y_train = cell(size(nSamples, 1), 1);
+
+% Generate training set
+tic
+for i = 1:nSamples
+    x0 = [0; 0];
+    rn = rand();
+    u_ = @(t) u(t, 1, rn);
+    u_vec = arrayfun(u_,t);
+    [t, x] = ode45(@(t, x) dx(t, x, u_(t), p), t, x0);
+
+    X_train{i} = [x0; t(:); u_vec(:)];
+    Y_train{i} = x(:);
+end
+fprintf('Time to generate training set: \n');
+toc
+
+% Set up Neural Hybrid
+% Everthing but the stiffness:
+Dynamics =@(p,t,x,u) [ ...
+  x(2);
+  1/p(1) * (u - p(2)*x(2))];
+
+
+Jacobian = @(p,t,x,u) deal( ...
+    [0 1; 0 -p(2)/p(1)], ...                     % dfdx
+    [ 0               0 ; ...                    % d/dp₁ d/dp₂
+      p(2)/p(1)^2   -1/p(1) ] );                 % dfdpf
+
+numParameters = 2;
+stateSize = 2;
+controlSize = 2;
+
+if use_adjoint
+  hnode_layer = HybridNeuralODE_adjoint(Dynamics, Jacobian, 2, 2, 1);
+  fprintf('Using adjoint method for training.\n');
+else
+  hnode_layer = HybridNeuralODE(Dynamics, 2, 2, 1);
+end
+
+input_layer = featureInputLayer(stateSize + 2*numel(t), 'Name', 'input');
+example_input = dlarray(X_train{1}, 'CB');
+net = dlnetwork([input_layer; hnode_layer], example_input);
+
+options = trainingOptions('adam', ...
+    'InitialLearnRate', LearningRate, ...
+    'MaxEpochs', Epochs, ...
+    'MiniBatchSize', 1, ...
+    'Verbose', true);
+  %  'Plots','training-progress');
+
+X_train_matrix = cat(2, X_train{:})';  % Each row is a sample
+Y_train_matrix = cat(2, Y_train{:})';  % Each row is a target
+
+tic
+
+figure; hold all;
+for i=1:nSamples
+  x_target = reshape(Y_train{i},size(x));
+  plot(t,x_target(:,1),'-','Color',cmap(i,:));
+end
+
+ps = gobjects(nTrainingRuns, numel(nSamples));
+for k=1:nTrainingRuns
+
+  % Decay Learning rate
+%  options.InitialLearnRate = options.InitialLearnRate / 2;
+
+  net = trainnet( ...
+    X_train_matrix, Y_train_matrix, net, 'mse', options);
+  save(['./net-',num2str(k),'.mat'],'net');
+
+  gcf; hold all;
+  title(['Training run:', num2str(k)]);
+  for i = 1:nSamples
+      Y = predict(net, X_train{i}');
+      x_model = reshape(Y, size(x));
+      ps(k,i) = plot(t,x_model(:,1),'--','Color',cmap(i,:));
+      if k > 1
+        color_ = ps(k,i).Color;
+        ps(k-1,i).Color = [color_, 0.1];
+      end
+  end
+  print('-dpng',['training_run-',num2str(k),'.png']);
+end
+toc
